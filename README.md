73df5e8afe7e6726cea191dd3908ce14e1e57ec1
James Cuzella <jcuzella@tangogroup.com>
2013-12-29 22:12:26 -0700

    Initial lyraphase-users cookbook

---
+++ README.md
@@ -0,0 +1,13 @@
+# users-lyraphase cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: James Cuzella (<james.cuzella@lyraphase.com>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'users-lyraphase'
+maintainer       'James Cuzella'
+maintainer_email 'james.cuzella@lyraphase.com'
+license          'Apache 2.0'
+description      'Installs/Configures users-lyraphase'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
