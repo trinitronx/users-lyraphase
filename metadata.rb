4317c606184091f633c07214cd11167fa30ca548
James Cuzella <james.cuzella@lyraphase.com>
2013-12-30 00:21:06 -0700

    Fixing cookbook dependencies

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,10 @@ maintainer_email 'james.cuzella@lyraphase.com'
 license          'Apache 2.0'
 description      'Sets up lyraphase defined users using the standard chef users cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
+depends 'users'
+depends 'ssh_known_hosts'
+
+# For use in vagrant and chef-solo, this is required. In chef-client, this cookbook is a no-op
+depends 'chef-solo-search'
