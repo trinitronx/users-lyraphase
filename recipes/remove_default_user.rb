087d17fed7494dacb0ab0fa7838751c215b49b52
James Cuzella <james.cuzella@lyraphase.com>
2016-02-17 21:11:56 -0700

    Autodetect username for uid 1000 from data bag; Enable users-lyraphase::remove_default_user to work with Chef Solo by defaulting to my user

--- recipes/remove_default_user.rb
+++ recipes/remove_default_user.rb
@@ -1,7 +1,17 @@
 users_by_id =  node['etc']['passwd'].map { |user| [ user[0], user[1]['uid'] ] }
 default_user =  users_by_id.find { |u| u[1] == 1000 }
 
-unless default_user.nil? || default_user.empty? || default_user[0] == 'trinitronx'
+unless Chef::Config[:solo]
+  defined_users =  data_bag('users').map { |u| {:uid => data_bag_item('users', u)['uid'], :name => data_bag_item('users', u)['id']} } }
+end
+
+if defined_users.nil? || defined_users.empty?
+  defined_user_1000 = {:uid=>1000, :name=>"trinitronx"}
+else
+  defined_user_1000 = defined_users.find { |u| u[:uid] == 1000 }
+end
+
+unless default_user.nil? || default_user.empty? || default_user[0] == defined_user_1000[:name]
   user default_user[0] do
     uid 1000
     action [ :remove ]
