a988705021e21f2df77abd7037124af872c9e479
James Cuzella <james.cuzella@lyraphase.com>
2016-02-17 20:36:40 -0700

    Adding recipe to remove default uid 1000 if already installed

---
+++ recipes/remove_default_user.rb
@@ -0,0 +1,9 @@
+users_by_id =  node['etc']['passwd'].map { |user| [ user[0], user[1]['uid'] ] }
+default_user =  users_by_id.find { |u| u[1] == 1000 }
+
+unless default_user.nil? || default_user.empty?
+  user default_user[0] do
+    uid 1000
+    action [ :remove ]
+  end
+end
