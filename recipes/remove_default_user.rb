7eb97fa4d3a08a329c821a98a027de2ec4e35d3b
James Cuzella <james.cuzella@lyraphase.com>
2016-02-17 20:43:24 -0700

    Avoid removing my user if it already exists

--- recipes/remove_default_user.rb
+++ recipes/remove_default_user.rb
@@ -1,7 +1,7 @@
 users_by_id =  node['etc']['passwd'].map { |user| [ user[0], user[1]['uid'] ] }
 default_user =  users_by_id.find { |u| u[1] == 1000 }
 
-unless default_user.nil? || default_user.empty?
+unless default_user.nil? || default_user.empty? || default_user[0] == 'trinitronx'
   user default_user[0] do
     uid 1000
     action [ :remove ]
