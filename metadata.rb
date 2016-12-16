6bf52207d16289e9d35ac88dc7b2f56a6bab42ee
James Cuzella <james.cuzella@lyraphase.com>
2016-12-15 23:02:53 -0700

    Bump cookbook version to v0.2.4

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'james.cuzella@lyraphase.com'
 license          'Apache 2.0'
 description      'Sets up lyraphase defined users using the standard chef users cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.3'
+version          '0.2.4'
 
 depends 'users'
 depends 'ssh_known_hosts'
